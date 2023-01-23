e74b9586197bfc52e017a8e61da592120dafae72
Huffer <hugoferreirait@gmail.com>
2023-01-23 15:58:41 -0300

    PORT error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const uuidv4 = require('uuid').v4;
 // Spinning the http server and the WebSocket server.
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
-const port = 8000;
+const port =  process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`WebSocket server is running on port ${port}`);
