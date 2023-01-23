8369dcda7213b5b54a18427019011bb8879965d9
Huffer <hugoferreirait@gmail.com>
2023-01-23 16:13:56 -0300

    chore: change to express

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const { WebSocketServer, WebSocket } = require("ws");
-const http = require("http");
-const uuidv4 = require('uuid').v4;
-// Spinning the http server and the WebSocket server.
-const server = http.createServer();
-const wsServer = new WebSocketServer({ server });
-const port =  process.env.PORT || 8000;
 
-server.listen(port, () => {
-  console.log(`WebSocket server is running on port ${port}`);
-});
+const express = require("express");
+const uuidv4 = require("uuid").v4;
+
+const PORT = process.env.PORT || 8000;
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wsServer = new WebSocketServer({ server });
 
 // I'm maintaining all active connections in this object
 const clients = {};
@@ -21,19 +22,19 @@ let userActivity = [];
 
 // Event types
 const typesDef = {
-  USER_EVENT: 'userevent',
-  CONTENT_CHANGE: 'contentchange'
-}
+  USER_EVENT: "userevent",
+  CONTENT_CHANGE: "contentchange",
+};
 
 function broadcastMessage(json) {
   // We are sending the current data to all connected clients
   const data = JSON.stringify(json);
-  for(let userId in clients) {
+  for (let userId in clients) {
     let client = clients[userId];
-    if(client.readyState === WebSocket.OPEN) {
+    if (client.readyState === WebSocket.OPEN) {
       client.send(data);
     }
-  };
+  }
 }
 
 function handleMessage(message, userId) {
@@ -51,26 +52,26 @@ function handleMessage(message, userId) {
 }
 
 function handleDisconnect(userId) {
-    console.log(`${userId} disconnected.`);
-    const json = { type: typesDef.USER_EVENT };
-    const username = users[userId]?.username || userId;
-    userActivity.push(`${username} left the document`);
-    json.data = { users, userActivity };
-    delete clients[userId];
-    delete users[userId];
-    broadcastMessage(json);
+  console.log(`${userId} disconnected.`);
+  const json = { type: typesDef.USER_EVENT };
+  const username = users[userId]?.username || userId;
+  userActivity.push(`${username} left the document`);
+  json.data = { users, userActivity };
+  delete clients[userId];
+  delete users[userId];
+  broadcastMessage(json);
 }
 
 // A new client connection request received
-wsServer.on('connection', function(connection) {
+wsServer.on("connection", function (connection) {
   // Generate a unique code for every user
   const userId = uuidv4();
-  console.log('Recieved a new connection');
+  console.log("Recieved a new connection");
 
   // Store the new connection and handle messages
   clients[userId] = connection;
   console.log(`${userId} connected.`);
-  connection.on('message', (message) => handleMessage(message, userId));
+  connection.on("message", (message) => handleMessage(message, userId));
   // User disconnected
-  connection.on('close', () => handleDisconnect(userId));
+  connection.on("close", () => handleDisconnect(userId));
 });
